b5558a8b4325bfccfa5ce538df270c62c10a81ea
Victor Andreasson <victor.andreasson@fripost.org>
2022-07-03 15:21:04 +0200

    Access key value is not sensitive

--- variables.tf
+++ variables.tf
@@ -1,7 +1,6 @@
 variable "otc_access_key" {
   description = "Access key for OTC user"
   type        = string
-  sensitive   = true
 }
 variable "otc_secret_key" {
   description = "Secret key for OTC user"
