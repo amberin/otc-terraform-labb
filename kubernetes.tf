995ff68de25ec36b58ccdab13cae130d22ca6173
Victor Andreasson <victor.andreasson@fripost.org>
2022-09-27 14:11:05 +0200

    Working shared bandwidth with LB and k8s cluster + node

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,8 +1,3 @@
-resource "opentelekomcloud_networking_secgroup_v2" "kubernetes" {
-  name        = "kubernetes"
-  description = "kubernetes"
-}
-
 resource "opentelekomcloud_vpc_v1" "kubernetes" {
   name = "kubernetes"
   cidr = "10.30.0.0/24"
@@ -15,31 +10,16 @@ resource "opentelekomcloud_vpc_subnet_v1" "kubernetes" {
   gateway_ip = "10.30.0.1"
 }
 
-resource "opentelekomcloud_vpc_eip_v1" "cluster1" {
-  publicip {
-    type = "5_bgp"
-  }
-  bandwidth {
-    name = "cluster1"
-    size = 1
-    share_type = "PER"
-  }
-}
-resource "opentelekomcloud_vpc_eip_v1" "cluster1-worker1" {
-  publicip {
-    type = "5_bgp"
-  }
-  bandwidth {
-    name = "cluster1-worker1"
-    size = 1
-    share_type = "PER"
-  }
-}
-
-
 resource "opentelekomcloud_swr_organization_v2" "victororg" {
   name = "victororg"
 }
+
+resource "opentelekomcloud_vpc_bandwidth_v2" "kubernetes" {
+  name = "kubernetes"
+  size = 5
+}
+
+resource "opentelekomcloud_networking_floatingip_v2" "cluster1" {}
 
 resource "opentelekomcloud_cce_cluster_v3" "cluster1" {
   name                   = "cluster1"
@@ -48,8 +28,10 @@ resource "opentelekomcloud_cce_cluster_v3" "cluster1" {
   vpc_id                 = opentelekomcloud_vpc_v1.kubernetes.id
   subnet_id              = opentelekomcloud_vpc_subnet_v1.kubernetes.id
   container_network_type = "vpc-router"
-  eip                    = opentelekomcloud_vpc_eip_v1.cluster1.publicip[0].ip_address
+  eip                    = opentelekomcloud_networking_floatingip_v2.cluster1.address
 }
+
+resource "opentelekomcloud_networking_floatingip_v2" "cluster1-node1" {}
 
 resource "opentelekomcloud_cce_node_v3" "cluster1-node1" {
   name              = "cluster1-node1"
@@ -62,7 +44,7 @@ resource "opentelekomcloud_cce_node_v3" "cluster1-node1" {
   # user "linux".
   key_pair  = opentelekomcloud_compute_keypair_v2.victors.id
 
-  eip_ids = [opentelekomcloud_vpc_eip_v1.cluster1-worker1.id]
+  eip_ids = [opentelekomcloud_networking_floatingip_v2.cluster1-node1.id]
 
   root_volume {
     # Use smallest possible disk size in dev
@@ -76,3 +58,26 @@ resource "opentelekomcloud_cce_node_v3" "cluster1-node1" {
     volumetype = "SATA"
   }
 }
+
+# Load balancer stuff
+resource "opentelekomcloud_networking_floatingip_v2" "loadbalancer1" {}
+
+resource "opentelekomcloud_lb_loadbalancer_v2" "loadbalancer1" {
+  name          = "loadbalancer1"
+  vip_subnet_id = opentelekomcloud_vpc_subnet_v1.kubernetes.subnet_id
+}
+
+resource "opentelekomcloud_networking_floatingip_associate_v2" "loadbalancer1" {
+  floating_ip = opentelekomcloud_networking_floatingip_v2.loadbalancer1.address
+  port_id     = opentelekomcloud_lb_loadbalancer_v2.loadbalancer1.vip_port_id
+}
+
+resource "opentelekomcloud_vpc_bandwidth_associate_v2" "kubernetes" {
+  bandwidth = opentelekomcloud_vpc_bandwidth_v2.kubernetes.id
+  floating_ips = [
+    opentelekomcloud_networking_floatingip_v2.cluster1.id,
+    opentelekomcloud_networking_floatingip_v2.cluster1-node1.id,
+    opentelekomcloud_networking_floatingip_v2.loadbalancer1.id
+  ]
+}
+
